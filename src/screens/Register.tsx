a27141c953f17d96bab2d0e8b56d8bf810615153
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-07-31 17:31:35 -0300

    Adicionando páginas de registro e detalhes da solicitação

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,9 +1,29 @@
 import { Text, VStack } from 'native-base';
+import { Header } from '../components/Header';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+
 
 export function Register() {
   return (
-    <VStack>
-      <Text>Register</Text>
+    <VStack flex={1} p={6} bg='gray.600'>
+      <Header title='Nova Solicitação'/>
+      <Input
+        placeholder='Número do patrimônio'
+        mt={4}
+      />
+      <Input
+        placeholder='Descrição do Problema'
+        flex={1}
+        mt={5}
+        multiline
+        textAlignVertical='top'
+      />
+
+      <Button
+        title='Cadastrar'
+        mt={5}
+      />
     </VStack>
   );
 }
